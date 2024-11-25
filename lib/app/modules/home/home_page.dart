51916b2b80428aab0e45413d52efa0898f7d238d
dong <17712102522@163.com>
2024-11-25 23:15:55 +0800

    添加一个头像

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -5,7 +5,6 @@ import 'package:app_demo/app/modules/home/widgets/custom_drawer_button_widget.da
 import 'package:app_demo/app/modules/home/widgets/custom_drawer_widget.dart';
 import 'package:app_demo/app/widgets/custom_button_language_widget.dart';
 import 'package:get/get.dart';
-import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 class HomePage extends GetView {
   HomePage({super.key});
@@ -18,29 +17,28 @@ class HomePage extends GetView {
 
   @override
   Widget build(BuildContext context) {
-    var renderBox =
-        _scaffoldKey.currentContext?.findRenderObject() as RenderBox?;
     return Scaffold(
       key: _scaffoldKey,
       drawer: CustomDrawer(),
-      appBar: TDNavBar(
-        useDefaultBack: false,
-        // titleWidget: TDImage(
-        //   assetUrl: 'assets/images/github.png',
-        // ),
-        leftBarItems: [
-          TDNavBarItem(
-            iconWidget: const TDAvatar(
-              size: TDAvatarSize.medium,
-              type: TDAvatarType.normal,
-              defaultUrl: 'assets/images/github.png',
+      appBar: AppBar(
+        leading: Container(
+          padding: const EdgeInsets.fromLTRB(16, 8, 0, 8),
+          child: CircleAvatar(
+            foregroundColor: Colors.red,
+            child: GestureDetector(
+              onTap: () {
+                _scaffoldKey.currentState?.openDrawer();
+              },
+              child: Image.asset(
+                'assets/images/avatar_girl.png',
+                width: 32,
+                height: 32,
+              ),
             ),
-            action: () {
-              TDToast.showText("点击图标", context: context);
-            },
           ),
-        ],
-        title: '首页',
+        ),
+        title: const Text('首页'),
+        centerTitle: true,
       ),
       body: Stack(
         children: <Widget>[
@@ -48,35 +46,29 @@ class HomePage extends GetView {
               child: Container(
             color: Colors.red,
             padding: const EdgeInsets.all(0),
-            child: const Center(
-              child: Text("Home"),
+            child: Center(
+              child: Container(
+                color: Colors.amberAccent,
+                width: 32,
+                height: 32,
+                child: CircleAvatar(
+                  radius: 32, // 设置头像的半径为11
+                  foregroundColor: Colors.red,
+                  child: Image.asset(
+                    'assets/images/github.png',
+                    width: 16,
+                    height: 16,
+                  ),
+                ),
+              ),
             ),
           )),
-          // SafeArea(
-          //     child: Obx(() => Container(
-          //           color: Colors.red,
-          //           padding: EdgeInsets.all(0),
-          //           child: this.screens[controller.screen],
-          //         )),
-          //         ),
           Positioned(
             top: 8,
             left: 5,
             child: SafeArea(
               child: CustomDrawerButtonWidget(
                   callback: () => _scaffoldKey.currentState?.openDrawer()
-                  // callback: () => TDDrawer(
-                  //       context,
-                  //       visible: true,
-                  //       drawerTop: renderBox?.size.height,
-                  //       items: List.generate(
-                  //               30, (index) => TDDrawerItem(title: '菜单$index'))
-                  //           .toList(),
-                  //       onItemClick: (index, item) {
-                  //         print(
-                  //             'drawer item被点击，index：$index，title：${item.title}');
-                  //       },
-                  //     )
                   ),
             ),
           ),
