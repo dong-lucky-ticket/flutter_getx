1629a3f834d2213303158eb1d78044ea3f34e5fa
dong <17712102522@163.com>
2024-12-07 12:19:58 +0800

    移除example代码以及用不到的组件

--- lib_back/app/theme/app_theme.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:app_demo/app/theme/app_colors.dart';
-final ThemeData appThemeData = ThemeData(
-  primaryColor: spotlightColor,
-  hintColor: softBlue
-);
